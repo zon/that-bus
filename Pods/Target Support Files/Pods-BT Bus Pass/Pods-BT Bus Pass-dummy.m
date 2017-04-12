39321b3486a050dc51796713a69ef7cfa935f7d0
Matthew Z Haralovich <zon@haralovich.com>
2017-04-12 16:15:45 -0400

    Moved iOS client to it's own directory.

--- Pods/Target Support Files/Pods-BT Bus Pass/Pods-BT Bus Pass-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BT_Bus_Pass : NSObject
-@end
-@implementation PodsDummy_Pods_BT_Bus_Pass
-@end
--- Pods/Target Support Files/Pods-BT Bus Pass/Pods-BT Bus Pass-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_BT_Bus_PassVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BT_Bus_PassVersionString[];
-
