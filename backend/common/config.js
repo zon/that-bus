4daa9af38e72d2b1ae672fcb84f35bd0e18c5905
Matthew Z Haralovich <zon@haralovich.com>
2017-04-14 09:35:49 -0400

    backend/config.json is now optional.

--- backend/common/config.js
+++ backend/common/config.js
@@ -3,6 +3,12 @@ var fs = require('fs')
 
 var configFile = path.normalize(__dirname +'/../config.json')
 
-var json = JSON.parse(fs.readFileSync(configFile, 'utf8'))
+var json = {}
+try {
+	json = JSON.parse(fs.readFileSync(configFile, 'utf8'))
+} catch (e) {
+	if (e.code != 'ENOENT' || e.errno != -4058)
+		throw e
+}
 
 module.exports = json
